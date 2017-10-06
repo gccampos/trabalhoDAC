bb915f01db34ea01adbba49cd539a2a4c8cf35da
Guilherme <guilherme@Guilherme-PC>
2017-10-06 19:17:10 -0300

    commit inicial

---
+++ src/main/java/com/trabalhoDAC/trabalhoDAC/TrabalhoDacApplication.java
@@ -0,0 +1,12 @@
+package com.trabalhoDAC.trabalhoDAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDacApplication.class, args);
+	}
+}
