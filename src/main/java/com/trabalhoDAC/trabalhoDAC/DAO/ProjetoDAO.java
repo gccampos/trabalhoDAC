af64352b01350b7d7fd62b6ba22a985eb91ecf1d
Sallé <jhonnyot@gmail.com>
2017-11-11 02:09:29 -0200

    1

--- src/main/java/com/trabalhoDAC/trabalhoDAC/DAO/ProjetoDAO.java
+++ src/main/java/com/trabalhoDAC/trabalhoDAC/DAO/ProjetoDAO.java
@@ -5,11 +5,13 @@
  */
 package com.trabalhoDAC.trabalhoDAC.DAO;
 
-import com.trabalhoDAC.trabalhoDAC.modelo.Projeto;
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.trabalhoDAC.trabalhoDAC.modelo.Projeto;
 
 /**
  *
@@ -18,15 +20,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProjetoDAO extends JpaRepository<Projeto, Long> {
 
-    @Query("select p from Projeto p where p.titulo=?1")
-    public Projeto buscaPorTitulo(String titulo);
+	@Query("select p from Projeto p where p.titulo=?1")
+	public Projeto buscaPorTitulo(String titulo);
 
-    @Query("select p from Projeto p where p.id_orientador=?1")
-    public Projeto buscaPorOrientador(Long professor);
+	@Query("select p from Projeto p where p.orientador=?1")
+	public Projeto buscaPorOrientador(Long professor);
 
-    @Query("select p from Projeto p where p.nota is null")
-    public List<Projeto> listarProjetosEmAndamento();
+	@Query("select p from Projeto p where p.nota is null")
+	public List<Projeto> listarProjetosEmAndamento();
 
-    @Query("select p from Projeto p where p.nota is not null")
-    public List<Projeto> listarProjetosConcluidos();
+	@Query("select p from Projeto p where p.nota is not null")
+	public List<Projeto> listarProjetosConcluidos();
 }
